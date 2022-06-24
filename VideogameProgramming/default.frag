cf23387b4e034392106c0a197a082d3f365c812c
Ferran Ruiz <ferriruiz3@gmail.com>
2022-06-24 09:40:06 +0200

    Shaders

--- VideogameProgramming/default.frag
+++ VideogameProgramming/default.frag
@@ -1,7 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 color;
+
+uniform vec3 tint;
+
 void main()
 {
-   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);
+   FragColor = vec4(color * tint, 1.0f);
 }
--- VideogameProgramming/main.cpp
+++ VideogameProgramming/main.cpp
@@ -1,21 +1,30 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include <math.h>
+#include <ctime>
+#include <chrono>
 
 #include "shaderClass.h"
 #include "VBO.h"
 #include "VAO.h"
 #include "EBO.h"
 
-// Vertices coordinates
+using std::cout; using std::endl;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::chrono::system_clock;
+
+// Colored vertices coordinates
 GLfloat vertices[] =
-{
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+{ //               COORDINATES                  /     COLORS           //
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
 };
 
 // Indices for vertices order
@@ -27,6 +36,9 @@ GLuint indices[] =
 };
 
 GLFWwindow* window; // Game window
+
+float t = 0;
+time_t current_time;
 
 void SetupGLFW() {
 
@@ -79,15 +91,22 @@ int main() {
 	EBO EBO1 = EBO(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
+	GLuint scale_uni = glGetUniformLocation(shaderProgram.ID, "scale");
+	GLuint tint_uni = glGetUniformLocation(shaderProgram.ID, "tint");
+
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f); //Clear back buffer with blue
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window); //Swap buffers
+
+	float t = 0;
+	float time = clock();
 
 	//Program core loop
 	while (!glfwWindowShouldClose(window)) {
@@ -96,6 +115,17 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		shaderProgram.Activate();
+
+		t += clock() - time;
+		time = clock();
+
+		std::cout << time << std::endl;
+
+		float osc = (sin(t / 1000) / 2) + 0.5f; //Value between 0 and 1
+
+		glUniform1f(scale_uni, osc);
+		glUniform3f(tint_uni, osc, 1.0f, 1.0f);
+
 		VAO1.Bind();
 
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0); //Interpret the information in the VAO as triangles, starting at index 0, and drawing 9 elements.
